7de234db9011daa90b277294ec15811113814d1a
Kyle Flores <Kflorespilot@yahoo.com>
2026-02-02 12:52:16 -0800

    Finalize CRUD endpoints and postman testing

--- src/app.js
+++ src/app.js
@@ -3,15 +3,21 @@ const creatureRoutes = require('./routes/creatureRoutes');
 
 const app = express();
 
-// Middleware to parse JSON request bodies
+// Middleware to parse JSON bodies
 app.use(express.json());
 
-// Simple health check route
+/*
+  Simple health-check route so I can confirm the server is running.
+  You can hit http://localhost:5000/ in the browser to see this.
+*/
 app.get('/', (req, res) => {
   res.json({ message: 'Shadehaven Lore API is running' });
 });
 
-// Creature routes
+/*
+  Creature routes
+  All creature-related endpoints will start with /api/creatures
+*/
 app.use('/api/creatures', creatureRoutes);
 
 module.exports = app;
